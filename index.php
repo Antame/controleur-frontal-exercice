b35966006da7ae0b22cb1fe6ea67de2db3611fa6
Antame <Anne.Martine.META.MUSASA@cf2000.onmicrosoft.com>
2021-10-29 22:30:27 +0200

    config.php et onfig.php.info

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 /*Controleur frontale*/
-création du conteleur frontal nommé index.php
-creation d'un .gitignore`` pour rendre invible config.php ( faire cette étape en premier lieu 
+## Etape 3 création du conteleur frontal nommé index.php
+## Creation d'un .gitignore`` pour rendre invible config.php ( faire cette étape en premier lieu 
 avant mm la cretation du controleur frontal pour assurer une 'sécurité'.
+## Etape 5 
+création du veritable config.php(il ne sera mis sur github grâce au .gitignore) (on peut créer une copie de config.php de laquelle on retirerait les informations sensibles ) enregistrer puis,
+(une fois, le config.php crée , l' enregistrer de nouveau sous le nom config.php.info``.
+on a ainsi 2 fichiers ( config.php et config.info.php))
