33f1cc2fb3fe6fe183f5a291ddc693e724ae8473
Antame <Anne.Martine.META.MUSASA@cf2000.onmicrosoft.com>
2021-10-29 18:03:22 +0200

    création du .gitignore withh config

---
+++ index.php
@@ -0,0 +1,4 @@
+/*Controleur frontale*/
+création du conteleur frontal nommé index.php
+creation d'un .gitignore`` pour rendre invible config.php ( faire cette étape en premier lieu 
+avant mm la cretation du controleur frontal pour assurer une 'sécurité'.
